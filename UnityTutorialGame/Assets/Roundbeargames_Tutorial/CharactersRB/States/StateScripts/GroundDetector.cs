2ec7f2b3386ef8c0d8883e978f0da5c28539ea58
RoundBearChoi <roundbeargames@gmail.com>
2019-04-21 00:54:12 +0900

    choi
    choi

--- UnityTutorialGame/Assets/Roundbeargames_Tutorial/CharactersRB/States/StateScripts/GroundDetector.cs
+++ UnityTutorialGame/Assets/Roundbeargames_Tutorial/CharactersRB/States/StateScripts/GroundDetector.cs
@@ -53,7 +53,10 @@ namespace roundbeargames_tutorial
                     RaycastHit hit;
                     if (Physics.Raycast(o.transform.position, -Vector3.up, out hit, Distance))
                     {
-                        return true;
+                        if (!control.RagdollParts.Contains(hit.collider))
+                        {
+                            return true;
+                        }
                     }
                 }
             }
