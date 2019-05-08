9ad37e934dab829f3b26ef15c3a2c0b65ec28e29
RoundBearChoi <roundbeargames@gmail.com>
2019-05-08 21:00:36 +0900

    choi
    choi

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/StateScripts/GravityPull.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/GravityPull")]
+    public class GravityPull : StateData
+    {
+        public AnimationCurve Gravity;
+
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.GravityMultiplier = Gravity.Evaluate(stateInfo.normalizedTime);
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.GravityMultiplier = 0f;
+        }
+    }
+}
