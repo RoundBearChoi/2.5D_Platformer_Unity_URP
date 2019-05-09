899ea7d8c8d2f3146c8528b912c2edbb3b691db2
RoundBearChoi <roundbeargames@gmail.com>
2019-05-09 22:07:24 +0900

    choi
    choi

--- 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/DamageDetector.cs
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/DamageDetector.cs
@@ -83,7 +83,7 @@ namespace roundbeargames_tutorial
 
         private void TakeDamage(AttackInfo info)
         {
-            CameraManager.Instance.ShakeCamera(0.3f);
+            CameraManager.Instance.ShakeCamera(0.2f);
 
             Debug.Log(info.Attacker.gameObject.name + " hits: " + this.gameObject.name);
             Debug.Log(this.gameObject.name + " hit in " + DamagedPart.ToString());
