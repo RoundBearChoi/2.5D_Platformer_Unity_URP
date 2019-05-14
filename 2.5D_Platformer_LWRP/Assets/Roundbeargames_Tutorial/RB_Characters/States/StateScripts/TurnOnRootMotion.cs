ef4bef03dafa6817fb1e2687076fe04da09be655
RoundBearChoi <roundbeargames@gmail.com>
2019-05-14 14:18:05 +0900

    choi
    choi

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/StateScripts/TurnOnRootMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/TurnOnRootMotion")]
+    public class TurnOnRootMotion : StateData
+    {
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.SkinnedMeshAnimator.applyRootMotion = true;
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.SkinnedMeshAnimator.applyRootMotion = false;
+        }
+    }
+}
