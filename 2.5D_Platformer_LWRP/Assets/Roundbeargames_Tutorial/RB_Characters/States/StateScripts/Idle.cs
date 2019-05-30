1e6f2e6a5d9851efa703f6ca1cdcd0f7ac0a438b
RoundBearChoi <roundbeargames@gmail.com>
2019-05-30 19:37:10 +0900

    choi
    choi

--- 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/StateScripts/Idle.cs
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/StateScripts/Idle.cs
@@ -28,12 +28,15 @@ namespace roundbeargames_tutorial
                 animator.SetBool(TransitionParameter.Jump.ToString(), true);
             }
 
-            if (control.MoveRight)
+            if (control.MoveLeft && control.MoveRight)
+            {
+                //do nothing
+            }
+            else if (control.MoveRight)
             {
                 animator.SetBool(TransitionParameter.Move.ToString(), true);
             }
-
-            if (control.MoveLeft)
+            else if (control.MoveLeft)
             {
                 animator.SetBool(TransitionParameter.Move.ToString(), true);
             }
