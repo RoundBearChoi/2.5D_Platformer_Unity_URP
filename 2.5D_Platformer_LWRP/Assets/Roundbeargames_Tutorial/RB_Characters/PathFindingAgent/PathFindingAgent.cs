44aefe6578754d0282c2fa8338b1a27030f68b95
RoundBearChoi <roundbeargames@gmail.com>
2019-06-07 16:09:18 +0900

    choi
    choi

--- 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/PathFindingAgent/PathFindingAgent.cs
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/PathFindingAgent/PathFindingAgent.cs
@@ -11,6 +11,10 @@ namespace roundbeargames_tutorial
         public GameObject target;
         NavMeshAgent navMeshAgent;
 
+        public Vector3 StartPosition;
+        public Vector3 EndPosition;
+        Coroutine Move;
+
         private void Awake()
         {
             navMeshAgent = GetComponent<NavMeshAgent>();
@@ -18,12 +22,40 @@ namespace roundbeargames_tutorial
 
         public void GoToTarget()
         {
+            navMeshAgent.isStopped = false;
+
             if (TargetPlayableCharacter)
             {
                 target = CharacterManager.Instance.GetPlayableCharacter().gameObject;
             }
 
             navMeshAgent.SetDestination(target.transform.position);
+
+            if (Move != null)
+            {
+                StopCoroutine(Move);
+            }
+
+            Move = StartCoroutine(_Move());
+        }
+
+        IEnumerator _Move()
+        {
+            while (true)
+            {
+                if (navMeshAgent.isOnOffMeshLink)
+                {
+                    StartPosition = transform.position;
+                    navMeshAgent.CompleteOffMeshLink();
+
+                    yield return new WaitForEndOfFrame();
+                    EndPosition = transform.position;
+                    navMeshAgent.isStopped = true;
+                    yield break;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
