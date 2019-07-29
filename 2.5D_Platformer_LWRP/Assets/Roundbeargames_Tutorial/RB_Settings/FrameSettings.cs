b060d08ec4d76c92a46bbbe877549bfd5282260c
RoundBearChoi <roundbeargames@gmail.com>
2019-07-30 00:54:32 +0900

    choi
    choi

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Settings/FrameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "Settings", menuName = "Roundbeargames/Settings/FrameSettings")]
+    public class FrameSettings : ScriptableObject
+    {
+        [Range(0.01f, 1f)]
+        public float TimeScale;
+        public int TargetFPS;
+    }
+}
---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Settings/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class Settings : MonoBehaviour
+    {
+        public FrameSettings frameSettings;
+
+        private void Awake()
+        {
+            Debug.Log("timeScale: " + frameSettings.TimeScale);
+            Time.timeScale = frameSettings.TimeScale;
+
+            Debug.Log("targetFrameRate: " + frameSettings.TargetFPS);
+            Application.targetFrameRate = frameSettings.TargetFPS;
+        }
+    }
+}
