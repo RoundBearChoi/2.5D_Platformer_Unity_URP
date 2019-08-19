a3b35c84a619c01ca552522578c9a2c18171b0dc
RoundBearChoi <roundbeargames@gmail.com>
2019-08-19 16:01:07 +0900

    choi
    choi

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/HierarchyLabel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Check out http://diegogiacomelli.com.br/unitytips-hierarchy-window-group-header/
+/// </summary>
+namespace Roundbeargames
+{
+    [InitializeOnLoad]
+    public class HierarchyLabel : MonoBehaviour
+    {
+        static HierarchyLabel()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        }
+
+        static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+        {
+            GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (obj != null && obj.name.StartsWith("---", System.StringComparison.Ordinal))
+            {
+                EditorGUI.DrawRect(selectionRect, Color.grey);
+                EditorGUI.DropShadowLabel(selectionRect, obj.name.Replace("-", "").ToString());
+            }
+        }
+    }
+}
