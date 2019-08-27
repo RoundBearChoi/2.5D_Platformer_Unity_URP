2a233fb10d3d7b3b885cfe88e69460827a9708e7
RoundBearChoi <roundbeargames@gmail.com>
2019-08-27 15:31:09 +0900

    choi
    choi

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_UI/PlayGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public class PlayGameButton : MonoBehaviour
+    {
+        public void OnClick_PlayGame()
+        {
+            //Debug.Log("clicked: PlayGame");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("TutorialScene_Sample_Day");
+        }
+    }
+}
