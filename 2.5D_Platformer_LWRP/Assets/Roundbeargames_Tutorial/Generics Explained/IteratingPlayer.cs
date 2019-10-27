2d08c51e0b8d0e9a15fc20b8e0b603da90949e1a
RoundBearChoi <roundbeargames@gmail.com>
2019-10-27 18:37:22 +0900

    generics explained

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/Generics Explained/IteratingPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IteratingPlayer : MonoBehaviour
+{
+    public List<int> IntList = new List<int>();
+    public List<string> StringList = new List<string>();
+
+    public void IterateThrough<T>(List<T> targetList)
+    {
+        foreach(T data in targetList)
+        {
+            Debug.Log(data);
+        }
+    }
+
+    private void Start()
+    {
+        IterateThrough(IntList);
+        IterateThrough(StringList);
+    }
+}
---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/Generics Explained/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon<T> : MonoBehaviour where T: MonoBehaviour
+{
+    void Start()
+    {
+        Declare();
+    }
+
+    void Declare()
+    {
+        Debug.Log("hello i am a " + typeof(T));
+
+        if (!this.gameObject.name.Contains("copy"))
+        {
+            GameObject copy = new GameObject();
+            copy.name = "copy of a " + typeof(T);
+
+            copy.AddComponent<T>();
+        }
+    }
+}
