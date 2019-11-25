2aa920bdd6e2ff92e842a982f8eb6696ffcb8ab3
RoundBearChoi <roundbeargames@gmail.com>
2019-11-25 20:44:49 +0900

    195 Importing an Environment (Part1) - LWRP

---
+++ 2.5D_Platformer_LWRP/Assets/ColliderRemover/ColliderRemover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public class ColliderRemover : MonoBehaviour
+    {
+        public void RemoveAllColliders()
+        {
+            Collider[] arr = this.gameObject.GetComponentsInChildren<Collider>();
+
+            foreach (Collider c in arr)
+            {
+                DestroyImmediate(c);
+            }
+        }
+    }
+}
---
+++ 2.5D_Platformer_LWRP/Assets/ColliderRemover/Editor/ColliderRemoverEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Roundbeargames
+{
+    [CustomEditor(typeof(ColliderRemover))]
+    public class ColliderRemoverEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Remove All Colliders"))
+            {
+                ColliderRemover rem = target as ColliderRemover;
+                rem.RemoveAllColliders();
+            }
+        }
+    }
+}
