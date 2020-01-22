0a52edf31449996047100e3b92b1fec0fe4e1a70
RoundBearChoi <roundbeargames@gmail.com>
2020-01-22 15:12:05 +0900

    231 Camera Shake & VFX for the Down Smash - LWRP

--- 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/Abilities_StateScripts/ShakeCamera.cs
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/Abilities_StateScripts/ShakeCamera.cs
@@ -9,35 +9,31 @@ namespace Roundbeargames
     {
         [Range(0f, 0.99f)]
         public float ShakeTiming;
+        public float ShakeLength;
 
         public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
             if (ShakeTiming == 0f)
             {
-                CameraManager.Instance.ShakeCamera(0.2f);
-
-                //CharacterControl control = characterState.GetCharacterControl(animator);
+                CameraManager.Instance.ShakeCamera(ShakeLength);
                 characterState.characterControl.animationProgress.CameraShaken = true;
             }
         }
 
         public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
-            //CharacterControl control = characterState.GetCharacterControl(animator);
-
             if (!characterState.characterControl.animationProgress.CameraShaken)
             {
                 if (stateInfo.normalizedTime >= ShakeTiming)
                 {
                     characterState.characterControl.animationProgress.CameraShaken = true;
-                    CameraManager.Instance.ShakeCamera(0.2f);
+                    CameraManager.Instance.ShakeCamera(ShakeLength);
                 }
             }
         }
 
         public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
-            //CharacterControl control = characterState.GetCharacterControl(animator);
             characterState.characterControl.animationProgress.CameraShaken = false;
         }
     }
