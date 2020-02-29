25ccd84f75623993b794277995d28b2b53e43b9a
RoundBearChoi <roundbeargames@gmail.com>
2020-02-29 16:48:48 +0900

    257 Shoryuken Transitions - URP

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/RB_Characters/States/Abilities_StateScripts/CheckAttackPress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/CheckAttackPress")]
+    public class CheckAttackPress : StateData
+    {
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            if (characterState.characterControl.Attack)
+            {
+                characterState.characterControl.SkinnedMeshAnimator.SetBool(
+                    HashManager.Instance.DicMainParams[TransitionParameter.Attack], true);
+            }
+            else
+            {
+                characterState.characterControl.SkinnedMeshAnimator.SetBool(
+                    HashManager.Instance.DicMainParams[TransitionParameter.Attack], false);
+            }
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+    }
+}
