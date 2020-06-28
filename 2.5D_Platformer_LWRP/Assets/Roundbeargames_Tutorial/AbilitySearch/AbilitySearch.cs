8c6ecb72503a621a65a7b13b55255ebde1ac90f8
RoundBearChoi <roundbeargames@gmail.com>
2020-06-28 15:24:52 +0900

    Devlog#5 - ability search

---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/AbilitySearch/AbilitySearch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilitySearch/AbilitySearch")]
+    public class AbilitySearch : ScriptableObject
+    {
+        public AnimatorController ControllerToSearch;
+        public StateData AbilityToSearch;
+        public List<List<CharacterState>> Lists;
+
+        public void Search()
+        {
+            CharacterState[] arr = ControllerToSearch.GetBehaviours<CharacterState>();
+
+            Lists = new List<List<CharacterState>>();
+
+            bool AbilityFound = false;
+            int totalUses = 0;
+
+            foreach (CharacterState state in arr)
+            {
+                foreach(StateData ability in state.ListAbilityData)
+                {
+                    if (ability == AbilityToSearch)
+                    {
+                        Debug.Log("---Ability Found---");
+                        foreach(StateData a in state.ListAbilityData)
+                        {
+                            Debug.Log(a.name);
+                        }
+
+                        totalUses++;
+                        AbilityFound = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!AbilityFound)
+            {
+                Debug.Log("Ability NOT found!");
+            }
+            else
+            {
+                Debug.Log("Total uses: " + totalUses);
+            }
+        }
+    }
+}
---
+++ 2.5D_Platformer_LWRP/Assets/Roundbeargames_Tutorial/AbilitySearch/Editor/AbilitySearchEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Roundbeargames
+{
+    [CustomEditor(typeof(AbilitySearch))]
+    public class AbilitySearchEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            AbilitySearch searcher = (AbilitySearch)target;
+
+            if (GUILayout.Button("Search Ability"))
+            {
+                searcher.Search();
+            }
+        }
+    }
+}
